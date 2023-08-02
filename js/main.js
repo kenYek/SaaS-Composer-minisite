904ef92f7f41ee01c3d3d4cb5e0777fa1eb8a862
ken.yek <ken.yek@advantech.com.tw>
2023-08-02 15:21:10 +0800

    update document

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@ window.onscroll = function () {
     scrollToTopBtn.style.display = 'none';
   }
 };
+
+const switchButton = document.getElementById('switchButton');
+let isMainColor = true;
+
+switchButton.addEventListener('click', () => {
+  isMainColor = !isMainColor;
+  const root = document.documentElement;
+  if (isMainColor) {
+    root.style.setProperty('--white', '#ffffff');
+    root.style.setProperty('--gray', 'rgb(226 226 226)');
+    root.style.setProperty('--gray1', '#ddd');
+    root.style.setProperty('--gray2', '#f2f2f2');
+    root.style.setProperty('--gray3', '#f5f5f5');
+    root.style.setProperty('--black', '#000000');
+    root.style.setProperty('--black1', '#333333');
+  } else {
+    root.style.setProperty('--white', '#000000');
+    root.style.setProperty('--gray', 'rgb(29 29 29)');
+    root.style.setProperty('--gray1', '#222');
+    root.style.setProperty('--gray2', '#0d0d0d');
+    root.style.setProperty('--gray3', '#0a0a0a');
+    root.style.setProperty('--black', '#ffffff');
+    root.style.setProperty('--black1', '#cccccc');
+  }
+  switchButton.classList.toggle('isMainColor', isMainColor);
+});
